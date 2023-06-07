f44f84421c7ce1b2293aff84d23de2ce9d08726e
JennGoldring <jenngoldring@yahoo.com>
2023-06-07 10:02:12 -0500

    troubleshooting failed tests

--- src/main/java/org/launchcode/techjobs/mvc/controllers/SearchController.java
+++ src/main/java/org/launchcode/techjobs/mvc/controllers/SearchController.java
@@ -32,7 +32,7 @@ public class SearchController {
     public String displaySearchResults(Model model, @RequestParam String searchType, @RequestParam String searchTerm) {
         ArrayList<Job> jobs;
 
-        if (searchTerm.toLowerCase().equals("all") || searchTerm.equals("")) {
+        if (searchTerm.toLowerCase().equals("all") || searchTerm.isEmpty()) {
             jobs = JobData.findAll();
         } else {
             jobs = JobData.findByColumnAndValue(searchType, searchTerm);
